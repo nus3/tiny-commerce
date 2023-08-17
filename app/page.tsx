5ba63979c92ded2c475cec59dc7e8a5a50faa63d
nus3 <hadayota33@gmail.com>
2023-08-17 17:55:02 +0900

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import { Carousel } from 'components/carousel';
 import { ThreeItemGrid } from 'components/grid/three-items';
+import Footer from 'components/layout/footer';
 import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <>
-      <h2>hello nus3</h2>
       <ThreeItemGrid />
       <Suspense>
         <Carousel />
+        <Suspense>
+          <Footer />
+        </Suspense>
       </Suspense>
     </>
   );
